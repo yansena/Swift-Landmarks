4da3b73cd5786d6f8e10df76f744c9986b91e153
C1325378 Yanderson Sena <C1325378@bb.com.br>
2021-12-31 12:35:59 -0300

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Yanderson Sena on 31/12/21.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
